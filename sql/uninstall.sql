9f4b45c420c579c9ec49b4825668e0f82a638295
Joel Jacobson <joel@gluefinance.com>
2010-12-31 13:58:07 +0100

    fix uninstall.sql

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -3,7 +3,6 @@ BEGIN;
 DROP SCHEMA snapshot CASCADE;
 DROP FUNCTION public.snapshot();
 DROP FUNCTION public.snapshot(bigint);
-DROP VIEW public.view_snapshots;
 DROP USER snapshot;
 DROP GROUP snapshot_group;
 
