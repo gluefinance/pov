020ad06550e8dd739bbe359e7bc35f26c2d7a8a9
Joel Jacobson <joel@gluefinance.com>
2011-01-01 12:46:05 +0100

    Hash each array element individually, then concat hashes and hash again. This fixes the unlikely but theoretical problem when two different arrays, such as ['ab','c'] and ['a','bc'] would result in the same hash.

--- sql/schema/fsnapshot/functions/Hash.sql
+++ sql/schema/fsnapshot/functions/Hash.sql
@@ -1,15 +1,19 @@
-CREATE OR REPLACE FUNCTION Hash(_Text text[]) RETURNS TEXT AS $BODY$
+CREATE OR REPLACE FUNCTION Hash(_Text text) RETURNS TEXT AS $BODY$
 DECLARE
 _Hash text;
 BEGIN
 PERFORM 1 FROM pg_proc WHERE proname = 'digest' AND probin = '$libdir/pgcrypto';
 IF FOUND THEN
     -- If we have contrib/pgcrypto, use SHA1
-    _Hash := encode(public.digest(array_to_string($1,''), 'sha1'), 'hex');
+    _Hash := encode(public.digest($1, 'sha1'), 'hex');
 ELSE
     -- Default to MD5
-    _Hash := md5(array_to_string($1,''));
+    _Hash := md5($1);
 END IF;
 RETURN _Hash;
 END;
 $BODY$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION Hash(_Text text[]) RETURNS TEXT AS $BODY$
+SELECT fsnapshot.Hash(array_to_string(array_agg(fsnapshot.Hash(unnest)),'')) FROM unnest($1)
+$BODY$ LANGUAGE sql IMMUTABLE;
