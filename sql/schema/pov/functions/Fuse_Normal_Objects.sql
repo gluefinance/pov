f137d3e2c2c22949bf15d1dbbb4a19eb50da964c
Joel Jacobson <joel@Joel-Jacobsons-iMac-2.local>
2011-01-11 00:08:43 +0100

    Toposort normal objects, not yet finnished

--- sql/schema/pov/functions/Fuse_Normal_Objects.sql
+++ sql/schema/pov/functions/Fuse_Normal_Objects.sql
@@ -49,7 +49,7 @@ LEFT JOIN FuseMap               ON (NewObjects.Obj    = ANY(FuseMap.Children))
 LEFT JOIN FuseMap AS FuseMapRef ON (NewObjects.RefObj = ANY(FuseMapRef.Children))
 ),
 DepDigraph AS (
-SELECT RefObj, Obj FROM NormalObjects
+SELECT DISTINCT RefObj, Obj FROM NormalObjects
 WHERE RefObj <> Obj
 ),
 DotFormat AS (
@@ -63,5 +63,58 @@ SELECT '    "'
 FROM DepDigraph
 UNION ALL
 SELECT '}'
+),
+TopoSort AS (
+    SELECT tsort(array_to_string(array_agg(RefObj || ';' || Obj),';'),';',0,'sub {$a cmp $b}') FROM DepDigraph
 )
-SELECT diagraph FROM DotFormat;
+SELECT * FROM TopoSort;
+
+
+
+
+WITH
+FuseMap AS (SELECT NormalObject, Children FROM Fuse_Normal_Objects()),
+UserObjects AS (
+SELECT
+    refclassid, refobjid, classid, objid FROM pg_depend WHERE deptype <> 'p'
+    AND (
+        refclassid IN ('pg_class'::regclass,'pg_constraint'::regclass,'pg_trigger'::regclass,'pg_proc'::regclass)
+        OR
+        classid IN ('pg_class'::regclass,'pg_constraint'::regclass,'pg_trigger'::regclass,'pg_proc'::regclass)
+    )
+    AND (
+    )
+WHERE pg_catalog.pg_namespace.nspname NOT IN ('pg_catalog','information_schema','pov')
+
+
+)
+Objects AS (
+SELECT
+    pg_describe_object(refclassid,refobjid,0) AS RefObj,
+    pg_describe_object(classid,objid,0) AS Obj
+FROM pg_depend WHERE deptype <> 'p'
+),
+NormalObjects AS (
+SELECT
+    COALESCE(FuseMapRef.NormalObject,Objects.RefObj) AS RefObj,
+    COALESCE(FuseMap.NormalObject,Objects.Obj) AS Obj
+FROM Objects
+LEFT JOIN FuseMap               ON (Objects.Obj    = ANY(FuseMap.Children))
+LEFT JOIN FuseMap AS FuseMapRef ON (Objects.RefObj = ANY(FuseMapRef.Children))
+),
+DepDigraph AS (
+SELECT DISTINCT RefObj, Obj FROM NormalObjects
+WHERE RefObj <> Obj
+),
+TopoSort AS (
+    SELECT tsort(array_to_string(array_agg(RefObj || ';' || Obj),';'),';',0,'DFS') FROM DepDigraph
+)
+SELECT * FROM TopoSort;
+
+
+
+
+
+
+
+
