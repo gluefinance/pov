26fa5d19370e5af227c107109b2019a99cdb1041
Joel Jacobson <joel@gluefinance.com>
2011-01-12 10:49:17 +0100

    explained problem in pg_depend.txt and created actual topograph manually

--- doc/pg_depend/pg_depend.sh
+++ doc/pg_depend/pg_depend.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 dot -opg_depend.png -Tpng pg_depend.dot
 dot -opg_depend.svg -Tsvg pg_depend.dot
+
+# Actual possible creation order
+dot -opg_depend_actual.png -Tpng pg_depend_actual.dot
+dot -opg_depend_actual.svg -Tsvg pg_depend_actual.dot
--- doc/pg_depend/pg_depend.sql
+++ doc/pg_depend/pg_depend.sql
@@ -1,38 +1,24 @@
 -- Store already existing dependencies, so we can ignore them in the generated graph
 INSERT INTO pg_depend_before ( classid, objid, objsubid, refclassid, refobjid, refobjsubid, deptype ) SELECT classid, objid, objsubid, refclassid, refobjid, refobjsubid, deptype FROM pg_depend;
 
+-- Dependency level 1 (directly under public schema:)
 CREATE TABLE t1 ( id integer, PRIMARY KEY (id) );
-CREATE TABLE t2 ( id integer, PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES t1(id) );
-CREATE VIEW v1 AS SELECT * FROM t1;
-CREATE VIEW v2 AS SELECT * FROM t2;
-CREATE VIEW v3 AS SELECT v1.id AS id1, v2.id AS id2 FROM v1, v2;
 CREATE FUNCTION f1 ( integer ) RETURNS boolean AS $$ SELECT $1 > 1; $$ LANGUAGE sql;
-CREATE VIEW v4 AS SELECT *, f1(id1) FROM v3;
 CREATE SEQUENCE s1;
+
+-- Dependency level 2:
+CREATE TABLE t2 ( id integer, PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES t1(id) );
 CREATE TABLE t3 ( id integer not null default nextval('s1'), PRIMARY KEY (id), CHECK(f1(id)) );
 
--- glue=# CREATE TABLE t1 ( id integer, PRIMARY KEY (id) );
--- NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "t1_pkey" for table "t1"
--- CREATE TABLE
--- glue=# CREATE TABLE t2 ( id integer, PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES t1(id) );
--- NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "t2_pkey" for table "t2"
--- CREATE TABLE
--- glue=# CREATE VIEW v1 AS SELECT * FROM t1;
--- CREATE VIEW
--- glue=# CREATE VIEW v2 AS SELECT * FROM t2;
--- CREATE VIEW
--- glue=# CREATE VIEW v3 AS SELECT v1.id AS id1, v2.id AS id2 FROM v1, v2;
--- CREATE VIEW
--- glue=# CREATE FUNCTION f1 ( integer ) RETURNS boolean AS $$ SELECT $1 > 1; $$ LANGUAGE sql;
--- CREATE FUNCTION
--- glue=# CREATE VIEW v4 AS SELECT *, f1(id1) FROM v3;
--- CREATE VIEW
--- glue=# CREATE SEQUENCE s1;
--- CREATE SEQUENCE
--- glue=# CREATE TABLE t3 ( id integer not null default nextval('s1'), PRIMARY KEY (id), CHECK(f1(id)) );
--- NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "t3_pkey" for table "t3"
--- CREATE TABLE
--- glue=# 
+-- Dependency level 3:
+CREATE VIEW v1 AS SELECT * FROM t1;
+CREATE VIEW v2 AS SELECT * FROM t2;
+
+-- Dependency level 4:
+CREATE VIEW v3 AS SELECT v1.id AS id1, v2.id AS id2 FROM v1, v2;
+
+-- Dependency level 5:
+CREATE VIEW v4 AS SELECT *, f1(id1) FROM v3;
 
 -- Generate .dot file, pg_depend.dot contains the output of the query
 WITH
