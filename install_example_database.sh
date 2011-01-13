8bab6d94e6e37dcf397a43a5b9ba999d2c0cacd6
Joel Jacobson <joel@gluefinance.com>
2011-01-13 23:58:02 +0100

    Fixed install scripts

--- install_example_database.sh
+++ install_example_database.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 dropdb test 2> /dev/null
 createdb test
-psql test -f 
-psql -f sql/uninstall.sql test 2> /dev/null
-psql -f sql/install.sql test
-psql -f test.sql test 1>test.stdout.tmp 2>test.stderr.tmp
-diff test.stdout test.stdout.tmp
-diff test.stderr test.stderr.tmp
+psql test -f sql/install_example_database.sql
+echo Installed!
--- sql/install_example_database.sql
+++ sql/install_example_database.sql
@@ -2,10 +2,11 @@ CREATE SCHEMA pov;
 CREATE LANGUAGE plperl;
 CREATE LANGUAGE plpgsql;
 \i sql/schema/pov/functions/_format_type.sql
+\i sql/schema/pov/functions/tsort.pl
+\i sql/schema/pov/functions/pg_get_object_unique_columns.sql
 \i sql/schema/pov/views/pg_all_objects_unique_columns.sql
 \i sql/schema/pov/views/pg_depend_remapped.sql
 \i sql/schema/pov/views/pg_depend_oid_concat.sql
-
-\i sql/schema/pov/functions/tsort.pl
-
-
+\i sql/schema/pov/views/pg_depend_dot.sql
+\i sql/schema/pov/views/pg_depend_tsort.sql
+\i sql/schema/pov/views/pg_depend_definitions.sql
