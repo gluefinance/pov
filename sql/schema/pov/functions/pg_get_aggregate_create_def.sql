c5bc5766c0206ed2b5806dcfba7a79ba0a563ad3
Joel Jacobson <joel@gluefinance.com>
2011-01-14 19:04:26 +0100

    Add namespace to aggregate def

--- sql/schema/pov/functions/pg_get_aggregate_create_def.sql
+++ sql/schema/pov/functions/pg_get_aggregate_create_def.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION pov.pg_get_aggregate_create_def(oid) RETURNS TEXT AS $BODY$
-SELECT 'CREATE AGGREGATE ' || pg_catalog.pg_proc.proname || '(' || pg_catalog.pg_get_function_identity_arguments(pg_catalog.pg_proc.oid) || E') (\n' ||
+SELECT 'CREATE AGGREGATE ' || pg_catalog.pg_namespace.nspname || '.' || pg_catalog.pg_proc.proname || '(' || pg_catalog.pg_get_function_identity_arguments(pg_catalog.pg_proc.oid) || E') (\n' ||
 '    SFUNC = ' || pg_catalog.pg_aggregate.aggtransfn::text || E',\n' ||
 '    STYPE = ' || pg_catalog.pg_aggregate.aggtranstype::regtype::text ||
 CASE WHEN pg_catalog.pg_aggregate.aggfinalfn::text = '-' THEN '' ELSE E',\n    FINALFUNC = ' || pg_catalog.pg_aggregate.aggfinalfn::text END ||
@@ -8,6 +8,7 @@ CASE WHEN pg_catalog.pg_operator.oprname IS NULL         THEN '' ELSE E',\n    S
 E'\n)'
 FROM pg_catalog.pg_proc
 JOIN pg_catalog.pg_aggregate     ON (pg_catalog.pg_aggregate.aggfnoid = pg_catalog.pg_proc.oid)
+JOIN pg_catalog.pg_namespace     ON (pg_catalog.pg_namespace.oid      = pg_catalog.pg_proc.pronamespace)
 LEFT JOIN pg_catalog.pg_operator ON (pg_catalog.pg_operator.oid       = pg_catalog.pg_aggregate.aggsortop)
 WHERE pg_catalog.pg_proc.oid = $1;
 $BODY$ LANGUAGE sql STABLE;
--- sql/schema/pov/functions/pg_get_aggregate_drop_def.sql
+++ sql/schema/pov/functions/pg_get_aggregate_drop_def.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION pov.pg_get_aggregate_drop_def(oid) RETURNS TEXT AS $BODY$
-SELECT 'DROP AGGREGATE ' || pg_catalog.pg_proc.proname || '(' || pg_catalog.pg_get_function_identity_arguments(pg_catalog.pg_proc.oid) || ')'
+SELECT 'DROP AGGREGATE ' || pg_catalog.pg_namespace.nspname || '.' || pg_catalog.pg_proc.proname || '(' || pg_catalog.pg_get_function_identity_arguments(pg_catalog.pg_proc.oid) || ')'
 FROM pg_catalog.pg_proc
 JOIN pg_catalog.pg_aggregate ON (pg_catalog.pg_aggregate.aggfnoid = pg_catalog.pg_proc.oid)
+JOIN pg_catalog.pg_namespace ON (pg_catalog.pg_namespace.oid      = pg_catalog.pg_proc.pronamespace)
 WHERE pg_catalog.pg_proc.oid = $1;
 $BODY$ LANGUAGE sql STABLE;
