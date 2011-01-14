7496572411638355023573df7684a75f00f9f5fd
Joel Jacobson <joel@gluefinance.com>
2011-01-14 17:54:19 +0100

    Restrict pov to only handle objects of type constraint, function, trigger and view

--- sql/schema/pov/functions/New_Revision.sql
+++ sql/schema/pov/functions/New_Revision.sql
@@ -16,7 +16,7 @@ SELECT Set_Revision(ObjectIDs) INTO STRICT _RevisionID FROM (
         -- Create new ObjectID for each function.
         -- If a object already exists with the same content (function definition), its ObjectID will be selected.
         SELECT Set_Object(ARRAY[create_definition,drop_definition],description) AS ObjectID FROM pov.pg_depend_definitions
-        WHERE create_definition IS NOT NULL AND drop_definition IS NOT NULL
+        WHERE create_definition IS NOT NULL AND drop_definition IS NOT NULL AND description ~ '^(constraint|function|trigger|view)'
         ORDER BY row_number
     ) AS Objects
 ) AS Revision;
